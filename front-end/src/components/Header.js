a30b9502dfb317ae54f51df831d2286ed01e28bf
codesbydre <gabaldonandrea@gmail.com>
2024-01-24 09:47:16 -0800

    update navbar to have dropdown

--- front-end/src/components/Header.js
+++ front-end/src/components/Header.js
@@ -18,17 +18,60 @@ function Header() {
           <Link className="navbar-brand" to="/">
             FinLit
           </Link>
-          <div className="collapse navbar-collapse">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavDropdown"
+            aria-controls="navbarNavDropdown"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavDropdown">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
                 <Link className="nav-link" to="/">
                   Home
                 </Link>
               </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/news">
+              <li className="nav-item dropdown">
+                <a
+                  className="nav-link dropdown-toggle"
+                  href="#"
+                  id="navbarDropdownMenuLink"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
                   News
-                </Link>
+                </a>
+                <ul
+                  className="dropdown-menu"
+                  aria-labelledby="navbarDropdownMenuLink"
+                >
+                  <li>
+                    <Link className="dropdown-item" to="/news">
+                      All News
+                    </Link>
+                  </li>
+                  <li>
+                    <Link className="dropdown-item" to="/news/business">
+                      Business
+                    </Link>
+                  </li>
+                  <li>
+                    <Link className="dropdown-item" to="/news/technology">
+                      Technology
+                    </Link>
+                  </li>
+                  <li>
+                    <Link className="dropdown-item" to="/news/general">
+                      General
+                    </Link>
+                  </li>
+                </ul>
               </li>
               {isAuthenticated && (
                 <li className="nav-item">
