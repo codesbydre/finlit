ce211f8f4be7cb422122858f00b0c47ab8acf15c
codesbydre <gabaldonandrea@gmail.com>
2024-01-25 11:47:39 -0800

    improve spacing and button styles on HomePage

--- front-end/src/pages/HomePage.js
+++ front-end/src/pages/HomePage.js
@@ -10,35 +10,36 @@ function HomePage() {
 
   return (
     <div className="container-lg mt-4">
-      <div className="p-5 mb-4 bg-light rounded-3 text-center">
-        <h1 className="display-4">
-          Welcome to FinLit
-          {isAuthenticated ? `, ${username}!` : "!"}
-        </h1>
-        <p className="lead">
+      <div className="p-5 mb-5 bg-light rounded-3 text-center">
+        <h1 className="display-4 mb-4">Welcome to FinLit</h1>
+        <p className="lead mb-4">
           Explore current news, take quizzes, and improve your financial
           knowledge!
         </p>
         {isAuthenticated ? (
-          <p>
-            <Link to="/news" className="btn btn-primary btn-lg mr-2">
+          <div>
+            <Link to="/news" className="btn btn-primary btn-lg me-2 mb-3">
               Explore News
             </Link>
-            &nbsp;
-            <Link to="/quizzes" className="btn btn-secondary btn-lg">
+            <Link
+              to="/quizzes"
+              className="btn btn-outline-secondary btn-lg mb-3"
+            >
               Take Quiz
             </Link>
-          </p>
+          </div>
         ) : (
-          <p>
-            <Link to="/login" className="btn btn-primary btn-lg mr-2">
+          <div>
+            <Link to="/login" className="btn btn-primary btn-lg me-2 mb-3">
               Login
             </Link>
-            &nbsp;
-            <Link to="/register" className="btn btn-secondary btn-lg">
+            <Link
+              to="/register"
+              className="btn btn-outline-secondary btn-lg mb-3"
+            >
               Register
             </Link>
-          </p>
+          </div>
         )}
       </div>
       <div className="row">
