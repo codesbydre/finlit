c606e1751fc9da57dd3bfa84f358c4f117e5f44d
codesbydre <gabaldonandrea@gmail.com>
2024-01-25 12:01:05 -0800

    update to NavLink from Link for navbar items to show active class for current page, increased horizontal padding for each nav item

--- front-end/src/components/Header.js
+++ front-end/src/components/Header.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 
 function Header() {
   const navigate = useNavigate();
@@ -26,9 +26,9 @@ function Header() {
       <header>
         <nav className="navbar navbar-expand-lg navbar-light bg-white">
           <div className="container-lg">
-            <Link className="navbar-brand" to="/">
+            <NavLink className="navbar-brand" to="/">
               FinLit
-            </Link>
+            </NavLink>
             <button
               className="navbar-toggler"
               type="button"
@@ -42,25 +42,38 @@ function Header() {
             </button>
             <div className="collapse navbar-collapse" id="navbarNavDropdown">
               <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                <li className="nav-item">
-                  <Link className="nav-link" to="/">
+                <li className="nav-item px-3">
+                  <NavLink
+                    className="nav-link"
+                    to="/"
+                    exact
+                    activeClassName="active"
+                  >
                     Home
-                  </Link>
+                  </NavLink>
                 </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/news">
+                <li className="nav-item px-3">
+                  <NavLink
+                    className="nav-link"
+                    to="/news"
+                    activeClassName="active"
+                  >
                     News
-                  </Link>
+                  </NavLink>
                 </li>
                 {isAuthenticated && (
-                  <li className="nav-item">
-                    <Link className="nav-link" to="/quizzes">
+                  <li className="nav-item px-3">
+                    <NavLink
+                      className="nav-link"
+                      to="/quizzes"
+                      activeClassName="active"
+                    >
                       Quizzes
-                    </Link>
+                    </NavLink>
                   </li>
                 )}
                 {isAuthenticated ? (
-                  <li className="nav-item">
+                  <li className="nav-item px-3">
                     <button
                       onClick={handleLogout}
                       className="btn btn-link nav-link"
@@ -71,15 +84,23 @@ function Header() {
                   </li>
                 ) : (
                   <>
-                    <li className="nav-item">
-                      <Link className="nav-link" to="/login">
+                    <li className="nav-item px-3">
+                      <NavLink
+                        className="nav-link"
+                        to="/login"
+                        activeClassName="active"
+                      >
                         Login
-                      </Link>
+                      </NavLink>
                     </li>
-                    <li className="nav-item">
-                      <Link className="nav-link" to="/register">
+                    <li className="nav-item px-3">
+                      <NavLink
+                        className="nav-link"
+                        to="/register"
+                        activeClassName="active"
+                      >
                         Register
-                      </Link>
+                      </NavLink>
                     </li>
                   </>
                 )}
