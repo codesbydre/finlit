05544b1bd240b32b2d4bb6e2a93b6ec818dfc2e8
codesbydre <gabaldonandrea@gmail.com>
2024-01-26 13:23:28 -0800

    add babel-preset-react-app as dependency to fix missing babel plugin error, add testing for CategoriesList

---
+++ front-end/src/components/CategoriesList.test.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import CategoriesList from "./CategoriesList";
+
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useNavigate: () => jest.fn(),
+}));
+
+test("renders categories list", () => {
+  render(<CategoriesList />, { wrapper: MemoryRouter });
+  expect(screen.getByText("Category")).toBeInTheDocument();
+  expect(screen.getByText("Business")).toBeInTheDocument();
+  expect(screen.getByText("Technology")).toBeInTheDocument();
+  expect(screen.getByText("Entertainment")).toBeInTheDocument();
+  expect(screen.getByText("General")).toBeInTheDocument();
+  expect(screen.getByText("Health")).toBeInTheDocument();
+  expect(screen.getByText("Science")).toBeInTheDocument();
+  expect(screen.getByText("Sports")).toBeInTheDocument();
+});
