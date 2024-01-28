f9ff0f06f95e93a0694bc5cb8bbd47ebbf0b246e
codesbydre <gabaldonandrea@gmail.com>
2024-01-28 12:24:31 -0800

    update server to listen on render's PORT environment variable

--- back-end/server.js
+++ back-end/server.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const app = require("./app");
-const port = 3001;
+const port = process.env.PORT || 3001;
 
 app.get("/", (req, res) => {
   res.send("Backend running!");
